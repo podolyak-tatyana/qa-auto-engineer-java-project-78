4ddade72a84beb9a8df561f32a7e461991e31f6f
podolyak-tatyana <podolyak.tatyana2309@gmail.com>
2026-01-19 12:26:50 +0200

    move all schemas to schemas package

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,53 @@
+package hexlet.code;
+
+import java.util.Map;
+
+public final class MapSchema extends BaseSchema<Map<?, ?>> {
+    private static final String CHECK_REQUIRED = "required";
+    private static final String CHECK_SIZEOF = "sizeof";
+    private static final String CHECK_SHAPE = "shape";
+
+    public MapSchema required() {
+        addCheck(CHECK_REQUIRED, value -> value != null);
+        return this;
+    }
+
+    public MapSchema sizeof(int size) {
+        addCheck(CHECK_SIZEOF, value -> {
+            if (!hasCheck(CHECK_REQUIRED) && value == null) {
+                return true;
+            }
+            if (value == null) {
+                return false;
+            }
+            return value.size() == size;
+        });
+        return this;
+    }
+
+    public MapSchema shape(Map<String, ? extends BaseSchema<?>> schemas) {
+        addCheck(CHECK_SHAPE, value -> {
+            // без required() null валиден
+            if (!hasCheck(CHECK_REQUIRED) && value == null) {
+                return true;
+            }
+            if (value == null) {
+                return false;
+            }
+
+            for (var entry : schemas.entrySet()) {
+                String key = entry.getKey();
+                BaseSchema schema = entry.getValue();
+
+                Object fieldValue = value.get(key);
+                if (!schema.isValid(fieldValue)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        return this;
+    }
+}
